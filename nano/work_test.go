c330e5edf0228d7e699b4516d351a4ffb2e20974
Cenk Alti <cenkalti@gmail.com>
2020-06-18 17:28:20 +0300

    update difficulty for v21

--- nano/work_test.go
+++ nano/work_test.go
@@ -11,8 +11,8 @@ import "testing"
 // }
 
 func BenchmarkGenerateWork(b *testing.B) {
-	workThreshold = 0xfff0000000000000
+	workThresholdForSend = 0xfff0000000000000
 	for n := 0; n < b.N; n++ {
-		GenerateWork("42473809202D318F7DFA794B277E78AE3824836A674B1B88BEC1BBC277A87D52")
+		GenerateWork("42473809202D318F7DFA794B277E78AE3824836A674B1B88BEC1BBC277A87D52", true)
 	}
 }
